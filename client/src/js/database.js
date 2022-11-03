80540d4ad31790e6996756956434153ace24b31e
Diego <diegopena2022@gmail.com>
2022-11-02 19:21:44 -0700

    added card rendering

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,3 +13,35 @@ export const initdb = async () => {
         }
     })
 }
+
+// Export a function we will use to GET to the database.
+export const getDb = async () => {
+    console.log('GET from the database');
+
+    const contactDb = await openDB('contact_db', 1);
+
+    const tx = contactDb.transaction('contacts', 'readonly');
+
+    const store = tx.objectStore('contacts');
+
+    const request = store.getAll();
+
+    const result = await request;
+    console.log('result.value', result);
+    return result;
+};
+
+export const postDb = async (name, email, phone, profile) => {
+    console.log('POST to the database');
+
+    const contactDb = await openDB('contact_db', 1);
+
+    const tx = contactDb.transaction('contacts', 'readwrite');
+
+    const store = tx.objectStore('contacts');
+
+    const request = store.add({ name: name, email: email, phone: phone, profile: profile });
+
+    const result = await request;
+    console.log('🚀 - data saved to the database', result);
+}
